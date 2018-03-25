56447f750a7e68b9348a80f17778dc269db9e6e7
tejesh.papineni <tejesh.papineni@gramener.com>
2018-03-25 12:17:31 +0530

    async await nodejs practice

---
+++ async_wait_practice/sequential_tasks.js
@@ -0,0 +1,75 @@
+
+var TaskRunner = class TaskRunner {
+    constructor() {
+    }
+}
+
+async function wrapper(task) {
+    function inner(task) {
+        return new Promise((resolve, reject) => {
+            task(resolve)
+        })
+    }
+    await inner(task)
+}
+
+TaskRunner.prototype.run = function (tasks) {
+    async function main() {
+        for (let i = 0; i < tasks.length; i++) {
+            await wrapper(tasks[i])
+        }
+    }
+    main()
+}
+
+
+function task1(done) {
+    console.log('Task 1: Started');
+    // console.log(done())
+    console.log("self.done")
+    // Some async operation follows  
+    setTimeout(function () {
+        console.log('Task 1: Finished\n');
+        done();
+    }, 500);
+};
+
+function task2(done) {
+    console.log('Task 2: Started');
+    // Some async operation follows  
+    setTimeout(function () {
+        console.log('Task 2: Finished\n');
+        done();
+    }, 500);
+};
+
+function task3(done) {
+    console.log('Task 3: Started');
+    // Some async operation follows  
+    setTimeout(function () {
+        console.log('Task 3: Finished\n');
+        done();
+    }, 500);
+};
+
+function task4(done) {
+    console.log('Task 4: Started');
+    // Some async operation follows  
+    setTimeout(function () {
+        console.log('Task 4: Finished\n');
+        done();
+    }, 500);
+};
+
+function task5(done) {
+    console.log('Task 5: Started');
+    // Some async operation follows  
+    setTimeout(function () {
+        console.log('Task 5: Finished\n');
+        done();
+    }, 500);
+};
+
+var task_runner = new TaskRunner();
+
+task_runner.run([task1, task2, task3, task4, task5]);
